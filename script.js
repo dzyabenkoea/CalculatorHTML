9a1c08a6fc34dc4089cc74702090d76772e58c68
DzybenkoEA <temp@mail.ru>
2022-08-20 09:15:58 +0300

    Рабочая версия

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('input')
 input.value = 0
 let memory = 0
+let prevOperator = ''
+let nextValue = false;
 
 document.querySelectorAll('button').forEach(
     el => {
@@ -9,22 +11,44 @@ document.querySelectorAll('button').forEach(
                 const key = event.target
                 key.classList.add('clicked')
                 if (input.value == 0) input.value = ''
-                
                 const isOperatorKey = key.dataset.operator !== undefined
-                if (isOperatorKey){
-                    switch(key.dataset.operator){
-                        case 'add': memory += Number(input.value); break;
-                        case 'substract': memory -= Number(input.value); break;
-                        case 'divide': memory /= Number(input.value); break;
-                        case 'multiply': memory *= Number(input.value); break;
+
+                if (isOperatorKey) {
+                    if (key.dataset.operator == 'evaluate') {
+                        if (prevOperator) {
+                            const result = operate(memory, input.value, prevOperator)
+                            memory = 0
+                            input.value = result
+                            nextValue = true
+                        }
                     }
-                    input.value = memory;
-                } else{
-                    input.value += el.textContent
+                    else {
+                        memory = input.value
+                        prevOperator = key.dataset.operator;
+                        nextValue = true;
+                    }
                 }
-
+                // Если введена цифра
+                else {
+                    // Если после операции вводится новое число очистить ввод
+                    if (nextValue) {
+                        input.value = key.textContent;
+                        nextValue = false;
+                    } else {
+                        input.value += key.textContent;
+                    }
+                }
+                el.ontransitionend = () => { el.classList.remove('clicked') }
             })
-        el.ontransitionend = () => { el.classList.remove('clicked') }
+
+        function operate(value1, value2, operator) {
+            value1 = Number(value1)
+            value2 = Number(value2)
+            switch (operator) {
+                case 'add': return value1 += value2;
+                case 'substract': return value1 -= value2;
+                case 'divide': return value1 /= value2;
+                case 'multiply': return value1 *= value2;
+            }
+        }
     })
-
-
